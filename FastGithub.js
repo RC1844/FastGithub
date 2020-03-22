a383428937fe3a0a93fcf69dfce43e2aa99eef2a
RC1844 <1844766234@qq.com>
2020-03-22 17:32:26 +0800

    更新啊啊啊啊啊

--- FastGithub.js
+++ FastGithub.js
@@ -2,12 +2,13 @@
 // @icon            https://github.githubassets.com/favicon.ico
 // @name            GitHub 镜像加速下载
 // @namespace       https://github.com/RC1844
-// @author          RC
+// @author          RC1844
+// @license         MIT License
 // @description     加速GitHub克隆和下载
-// @include           *://github.com/*
-// @include           *://github*
+// @include         *://github.com/*
+// @include         *://github*
 // @require         http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
-// @version         1.1.4
+// @version         1.2.0
 // @grant           GM_addStyle
 // ==/UserScript==
 
@@ -20,65 +21,105 @@
   var depth = true;
   // var depth = false;
 
-  var loca = window.location.href;
-  var mirror_url1 = 'https://' + 'github.com.cnpmjs.org';
-  var mirror_url2 = 'http://' + 'github-mirror.bugkiller.org';
-  var mirror_url3 = 'https://' + 'github.wuyanzheshui.workers.dev';
-  var str1 = '';
+  var mirror_url = new Array();
+  mirror_url[0] = 'https://' + 'github.com.cnpmjs.org';
+  mirror_url[1] = 'http://' + 'github-mirror.bugkiller.org';
+  mirror_url[2] = 'https://' + 'github.wuyanzheshui.workers.dev';
 
+  var str1 = '';
   if (clone) {
     str1 += "git clone ";
     if (depth) {
       str1 += "--depth=1 ";
     }
   }
-  
+  var loca = window.location.href;
   var a = loca.split("/");
   var str2 = '/' + a[3] + '/' + a[4] + '.git';
-  var clone_utl1 = str1 + mirror_url1 + str2;
-  var clone_utl2 = str1 + mirror_url2 + str2;
   var str3 = window.location.pathname;
-  var web_url1 = mirror_url1 + str3;
-  var web_url2 = mirror_url2 + str3;
-  var web_url3 = mirror_url3 + str3;
 
-  var info = `
-  <details class="ment">
-  <summary><b>镜像网站(点击显示)</b></summary>
-  <div style="border:2px dashed ;">
-      <center>
-          <div>快速克隆1:<input type="text" data-autoselect="" value="${clone_utl1}" aria-label="将此存储库克隆到 ${clone_utl1}"
-                  readonly="" style="width:400px"> &nbsp;快速浏览1:<a href="${web_url1}" id="down">跳转</a></div>
-          <div>快速克隆2:<input type="text" data-autoselect="" value="${clone_utl2}" aria-label="将此存储库克隆到 ${clone_utl2}"
-                  readonly="" style="width:400px"> &nbsp;快速浏览2:<a href="${web_url2}" id="down">跳转</a></div>
-          <div><del>快速克隆3:</del><input type="text" data-autoselect="" value="clone、depth命令的插入可手动编辑代码关闭" aria-label="" readonly=""
-                  style="width:400px"> &nbsp;快速浏览3:<a href="${web_url3}" id="down">跳转</a></div>
-      </center>
-  </div>
-  </details>
-  </br>
-  `;
+  var clone_url = new Array();
+  var web_url = new Array();
+  for (let i = 0; i < mirror_url.length; i++) {
+    clone_url[i] = str1 + mirror_url[i] + str2;
+    web_url[i] = mirror_url[i] + str3;
+  }
+
+  //镜像列表
+  var info = `<details class="details-reset details-overlay branch-select-menu ">
+  <summary class="btn btn-sm btn-primary" title="镜像列表" aria-haspopup="menu" role="button">
+      <b>镜像网站</b>
+      <span class="dropdown-caret"></span>
+  </summary>
+  <details-menu class="SelectMenu">
+      <div style="width:500px;margin: 4px 0 16px;font-size: 12px; pointer-events: auto;border: 1px solid #d1d5da;position: absolute;top: 100%;left: 0;z-index: 100;padding-top: 4px;padding-bottom: 4px;background-color: #fff;background-clip: padding-box;border-radius: 4px;box-shadow: 0 1px 5px rgba(27,31,35,.15);">
+          <header style="flex: none;padding: 16px; padding-top: 8px;padding-bottom: 8px;">
+          <div class=" btn-block"
+              style="padding: 4px;border: 1px solid #d1d5da;background-color: #fff3cd;border-top-left-radius: 3px;border-top-right-radius: 3px;"
+              role="alert">clone、depth命令的插入可手动编辑代码关闭</div>`;
+
+  for (let i = 0; i < clone_url.length - 1; i++) {
+    info += `<div class="input-group">
+    <input type="text" class="form-control input-monospace input-sm"
+        value="${clone_url[i]}" data-autoselect="" readonly="">
+    <div class="input-group-button">
+        <clipboard-copy value="${clone_url[i]}" class="btn btn-sm">
+            <svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true">
+                <path fill-rule="evenodd"
+                    d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z">
+                </path>
+            </svg>
+        </clipboard-copy>
+    </div>
+</div>`;
+  }
+
+  info += `<div class="mt-2 d-flex">`;
+  for (let i = 0; i < web_url.length; i++) {
+    info += `<a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow"
+  href="${web_url[i]}">镜像浏览${i}</a>`;
+  }
+  if (location.hostname != "github.com") {
+    info += `<a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow"
+  href="https://github.com${str3}">返回GitHub</a>`
+  }
+  info += `</div></header></div></details-menu></details>`;
 
   $('.repository-content').prepend(info);
-  $('.release-entry').each(function () {
-    $(this).find('.Box-body>a').each(function () {
-      var href = $(this).attr('href');
-      var url1 = mirror_url2 + href;
-      var url2 = mirror_url3 + href;
-      var span1 = `<li><a href="${url1}" rel="nofollow"><span class="pl-2 flex-auto min-width-0 text-bold">快速下载1</span></a></li>`;
-      var span2 = `<li><a href="${url2}" rel="nofollow"><span class="pl-2 flex-auto min-width-0 text-bold">快速下载2</span></a></li>`;
 
-      $(this).after(span2);
-      $(this).after(span1);
-    });
-  });
+
+  //Fast Download ZIP
   $('.get-repo-modal-options').each(function () {
-    $(this).find('.btn-block,.js-anon-download-zip-link').each(function () {
+    $(this).find("a[rel='nofollow']").each(function () {
       var href = $(this).attr('href');
-      var url1 = mirror_url3 + href;
-      var span1 = `<a class="btn btn-outline get-repo-btn btn-block" rel="nofollow" href="${url1}">Fast Download ZIP</a>`;
+      var url1 = mirror_url[2] + href;
+      var span1 = `<a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow" href="${url1}">Fast Download ZIP</a>`;
 
-      $(this).after(span1);
+      $('.get-repo-modal-options').after(span1);
     });
   });
+
+
+  //Download Releases
+  $('.release-entry').each(function () {
+    $(this).find('.d-flex.Box-body>a').each(function () {
+      var href = $(this).attr('href');
+      var span = `<div style=" position: absolute;left: 65%;">`;
+      for (let i = 1; i < mirror_url.length; i++) {
+        span += `<a class="flex-1 btn btn-outline get-repo-btn" rel="nofollow" href="${mirror_url[i] + href}">快速下载${i}</a>`;
+      }
+      span += `</div>`
+
+      $(this).after(span);
+    });
+  });
+  $('.release-entry').each(function () {
+    $(this).find('.d-block.Box-body>a').each(function () {
+      var href = $(this).attr('href');
+      var span = `<a class="btn btn-outline get-repo-btn" rel="nofollow" href="${mirror_url[2] + href}">快速下载</a>`;
+
+      $(this).after(span);
+    });
+  });
+
 })();
--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 本人并不会js，所以只能改别人的代码
 
 该脚本在[GitHub 镜像加速下载-码酷博客-改](https://greasyfork.org/zh-CN/scripts/391911-github-%E9%95%9C%E5%83%8F%E5%8A%A0%E9%80%9F%E4%B8%8B%E8%BD%BD-%E7%A0%81%E9%85%B7%E5%8D%9A%E5%AE%A2-%E6%94%B9)基础上修改：
+v1.2.0
+1. 修复Fast Download ZIP无法注入的bug
+2. 修复取消一个无效的下载按钮
+3. 增加克隆链接复制按钮
+4. 增加浏览镜像站点返回GitHub的按钮
+5. Html代码全面回炉重造，增加修饰，按钮对齐，体验更佳
+
+v1.0-1.1.4
 1. 更换两个GitHub克隆镜像站点
 2. 改下载为跳转到镜像站点
 3. 提供3个可浏览镜像站点
@@ -13,14 +21,19 @@
 
 下载安装地址：https://raw.githubusercontent.com/RC1844/FastGithub/master/FastGithub.js
 
-<figure class="half">
-    <img src="REANDME\releases1.png" width="600">
-    <img src="REANDME\releases2.png" width="600">
-</figure>
-
 ### 已知的GitHub镜像
 |域名|https|克隆加速|zip加速|releases加速
 |:---:|:----------:|:--------:|:---------:|:---------:|
 |https://github.com.cnpmjs.org|✓|?✓|✗|✗
 |http://github-mirror.bugkiller.org|✗|✓|✗|✓
 |https://github.wuyanzheshui.workers.dev|✓|✗|✓|✓
+
+<figure class="half">
+    <img src="REANDME\releases1.png" width="600">
+    <img src="REANDME\releases2.png" width="600">
+</figure>
+<figure class="half">
+    <img src="REANDME\releases3.png" width="600">
+    <img src="REANDME\releases4.png" width="600">
+</figure>
+
