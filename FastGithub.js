539471ca661e0c0fda2d48efdb8ae0d9de472bd5
RC1844 <1844766234@qq.com>
2020-05-21 14:58:58 +0800

    v1.3.1
    Signed-off-by: RC1844 <1844766234@qq.com>

--- FastGithub.js
+++ FastGithub.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @icon            https://github.githubassets.com/favicon.ico
-// @name            GitHub 镜像加速访问、下载
-// @namespace       https://github.com/RC1844
-// @author          RC1844
-// @license         MIT License
-// @description     加速GitHub访问、克隆和下载
-// @include         *://github.com/*
-// @include         *://github*
-// @include         *://hub.fastgit.org/*
-// @require         http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
-// @version         1.2.5
-// @grant           GM_addStyle
+// @icon https://github.githubassets.com/favicon.ico
+// @name GitHub 镜像加速访问、下载
+// @namespace https://github.com/RC1844
+// @author RC1844
+// @license MIT License
+// @description 加速GitHub访问、克隆和下载
+// @include *://github.com/*
+// @include *://github*
+// @include *://hub.fastgit.org/*
+// @require http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
+// @version 1.3.1
+// @grant GM_addStyle
 // ==/UserScript==
 
 (function () {
@@ -24,8 +24,15 @@
 
   var mirror_url = new Array();
   mirror_url[0] = 'https://' + 'github.com.cnpmjs.org';
-  mirror_url[1] = 'http://' + 'hub.fastgit.org';
+  mirror_url[1] = 'https://' + 'hub.fastgit.org';
   mirror_url[2] = 'https://' + 'github.wuyanzheshui.workers.dev';
+  mirror_url[3] = 'https://' + 'github.bajins.com';
+  mirror_url[4] = 'https://' + 'download.fastgit.org';
+  mirror_url[5] = 'https://' + 'github.wuyanzheshui.workers.dev';
+  //添加对应数值即可使用
+  var clone_set = [0, 1];
+  var mirror_set = [0, 1, 2, 3, 5];
+  var download_set = [2, 4, 5];
 
   var str1 = '';
   if (clone) {
@@ -39,70 +46,72 @@
   var str2 = '/' + a[3] + '/' + a[4] + '.git';
   var str3 = window.location.pathname;
 
-  var clone_url = new Array();
-  var web_url = new Array();
-  for (let i = 0; i < mirror_url.length; i++) {
-    clone_url[i] = str1 + mirror_url[i] + str2;
-    web_url[i] = mirror_url[i] + str3;
-  }
-
   //镜像列表
   var info = `<details class="details-reset details-overlay branch-select-menu ">
   <summary class="btn btn-sm btn-primary" title="镜像列表" aria-haspopup="menu" role="button">
-      <b>镜像网站</b>
-      <span class="dropdown-caret"></span>
+    <b>镜像网站</b>
+    <span class="dropdown-caret"></span>
   </summary>
   <details-menu class="SelectMenu">
-      <div style="width:500px;margin: 4px 0 16px;font-size: 12px; pointer-events: auto;border: 1px solid #d1d5da;position: absolute;top: 100%;left: 0;z-index: 100;padding-top: 4px;padding-bottom: 4px;background-color: #fff;background-clip: padding-box;border-radius: 4px;box-shadow: 0 1px 5px rgba(27,31,35,.15);">
-          <header style="flex: none;padding: 16px; padding-top: 8px;padding-bottom: 8px;">
-          <div class=" btn-block"
-              style="padding: 4px;background-color: #ffffdd;color: #996600;border-top-left-radius: 3px;border-top-right-radius: 3px;"
-              role="alert">clone、depth命令的插入可手动编辑代码关闭</div>
-              <div class=" btn-block"
-              style="padding: 4px;background-color: #ffcccc;color: #990000;border-top-left-radius: 3px;border-top-right-radius: 3px;"
-              role="alert">请不要在镜像网站登录账号，若因此造成任何损失本人概不负责</div>`;
+    <div
+      style="width:500px;margin: 4px 0 16px;font-size: 12px; pointer-events: auto;border: 1px solid #d1d5da;position: absolute;top: 100%;left: 0;z-index: 100;padding-top: 4px;padding-bottom: 4px;background-color: #fff;background-clip: padding-box;border-radius: 4px;box-shadow: 0 1px 5px rgba(27,31,35,.15);">
+      <header style="flex: none;padding: 16px; padding-top: 8px;padding-bottom: 8px;">
+        <div class=" btn-block"
+          style="padding: 4px;background-color: #ffffdd;color: #996600;border-top-left-radius: 3px;border-top-right-radius: 3px;"
+          role="alert">clone、depth命令的插入可手动编辑代码关闭</div>
+        <div class=" btn-block"
+          style="padding: 4px;background-color: #ffcccc;color: #990000;border-top-left-radius: 3px;border-top-right-radius: 3px;"
+          role="alert">请不要在镜像网站登录账号，若因此造成任何损失本人概不负责</div>`;
 
-  for (let i = 0; i < clone_url.length - 1; i++) {
+  //克隆列表
+  for (let i in clone_set) {
     info += `<div class="input-group">
-    <input type="text" class="form-control input-monospace input-sm"
-        value="${clone_url[i]}" data-autoselect="" readonly="">
-    <div class="input-group-button">
-        <clipboard-copy value="${clone_url[i]}" class="btn btn-sm">
-            <svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true">
+          <input type="text" class="form-control input-monospace input-sm"
+            value="${str1 + mirror_url[clone_set[i]] + str2}" data-autoselect="" readonly="">
+          <div class="input-group-button">
+            <clipboard-copy value="${str1 + mirror_url[clone_set[i]] + str2}" class="btn btn-sm">
+              <svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="16"
+                aria-hidden="true">
                 <path fill-rule="evenodd"
-                    d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z">
+                  d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z">
                 </path>
-            </svg>
-        </clipboard-copy>
-    </div>
-</div>`;
+              </svg>
+            </clipboard-copy>
+          </div>
+        </div>`;
   }
 
+  //浏览列表
   info += `<div class="mt-2 d-flex">`;
-  for (let i = 0; i < web_url.length; i++) {
+  for (let i in mirror_set) {
     info += `<a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow"
-  href="${web_url[i]}">镜像浏览${i}</a>`;
+            href="${mirror_url[mirror_set[i]] + str3}">镜像浏览${i}</a>`
   }
   if (location.hostname != "github.com") {
     info += `<a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow"
-  href="https://github.com${str3}">返回GitHub</a>`
+            href="https://github.com${str3}">返回GitHub</a>`
   }
-  info += `</div></header></div></details-menu></details>`;
 
+  info += `</div>
+      </header>
+    </div>
+  </details-menu>
+</details>`;
   $('.repository-content').prepend(info);
 
 
   //Fast Download ZIP
-  $('.get-repo-modal-options').each(function () {
+  $('get-repo-controller > details > div > div > div > div.mt-2.d-flex').each(function () {
+    var span = `<div class="mt-2 d-flex">`;
     $(this).find("a[rel='nofollow']").each(function () {
       var href = $(this).attr('href');
-      var url1 = mirror_url[2] + href;
-      var url2 = 'https://download.fastgit.org' + href;
-      var span1 = `<div class="mt-2 d-flex"><a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow" href="${url1}">Fast Download ZIP1</a>
-                                            <a class="flex-1 btn btn-outline get-repo-btn btn-block" rel="nofollow" href="${url2}">Fast Download ZIP2</a></div>`;
-
-      $('.get-repo-modal-options').after(span1);
+      for (let i in download_set) {
+        span += `<a class="btn btn-outline get-repo-btn btn-block" rel="nofollow"
+    href="${mirror_url[download_set[i]] + href}">Download ZIP${i}</a>`;
+      }
+      span += `</div>`;
     });
+    $(this).after(span);
   });
 
 
@@ -111,22 +120,22 @@
     $(this).find('.d-flex.Box-body>a').each(function () {
       var href = $(this).attr('href');
       var span = `<div style=" position: absolute;left: 65%;">`;
-      // for (let i = 1; i < mirror_url.length; i++) {
-      var i=1;
-        span += `<a class="flex-1 btn btn-outline get-repo-btn" rel="nofollow" href="${mirror_url[2] + href}">快速下载${i}</a>
-                 <a class="flex-1 btn btn-outline get-repo-btn" rel="nofollow" href="${'https://download.fastgit.org' + href}">快速下载${2}</a>`;
-      // }
-      span += `</div>`
-
+      for (let i in download_set) {
+        span += `<a class="flex-1 btn btn-outline get-repo-btn" rel="nofollow"
+    href="${mirror_url[download_set[i]] + href}">快速下载${i}</a>`;
+      }
+      span += `</div>`;
       $(this).after(span);
     });
   });
   $('.release-main-section').each(function () {
     $(this).find('.d-block.Box-body>a').each(function () {
       var href = $(this).attr('href');
-      var span = `<a class="btn btn-outline get-repo-btn" rel="nofollow" href="${mirror_url[2] + href}">快速下载1</a>
-                  <a class="btn btn-outline get-repo-btn" rel="nofollow" href="${'https://download.fastgit.org' + href}">快速下载2</a>`;
-
+      var span = ``;
+      for (let i in download_set) {
+        span += `<a class="flex-1 btn btn-outline get-repo-btn" rel="nofollow"
+  href="${mirror_url[download_set[i]] + href}">快速下载${i}</a>`;
+      }
       $(this).after(span);
     });
   });
--- README.md
+++ README.md
@@ -7,6 +7,39 @@
 
 该脚本在[GitHub 镜像加速下载-码酷博客-改](https://greasyfork.org/zh-CN/scripts/391911-github-%E9%95%9C%E5%83%8F%E5%8A%A0%E9%80%9F%E4%B8%8B%E8%BD%BD-%E7%A0%81%E9%85%B7%E5%8D%9A%E5%AE%A2-%E6%94%B9)基础上修改：
 
+下载安装地址：https://raw.githubusercontent.com/RC1844/FastGithub/master/FastGithub.js
+
+或：[Greasyfork](https://greasyfork.org/zh-CN/scripts/397419-github-%E9%95%9C%E5%83%8F%E5%8A%A0%E9%80%9F%E4%B8%8B%E8%BD%BD)
+
+<figure class="half">
+    <img src="REANDME\releases1.png" width="600">
+    <img src="REANDME\releases2.png" width="600">
+</figure>
+<figure class="half">
+    <img src="REANDME\releases3.png" width="600">
+    <img src="REANDME\releases4.png" width="600">
+</figure>
+
+### 已知的GitHub镜像(含失效站点)
+|域名|https|克隆加速|zip加速|releases加速|主机服务商|服务器所在地
+|:---:|:----------:|:--------:|:---------:|:---------:|:---------:|:---------:|
+|https://github.com.cnpmjs.org|✓|?✓|✗|✗|dnspod|香港
+|~~http://github-mirror.bugkiller.org~~|✗|✓|✗|✓|?|日本
+|https://github.wuyanzheshui.workers.dev|✓|✗|✓|✓|Cloudflare|美国
+|https://github.bajins.com|✓|✗|?|?|Cloudflare|美国
+|https://doc.fastgit.org|✓|✓|✓|✓|fastgit|香港
+
+### 更新日志
+v1.3.1
+1. 取消新提供的下载站点
+2. 增加自建站点https://github.rc1844.workers.dev/，与https://github.wuyanzheshui.workers.dev相同
+3. 自建方法请看下方
+
+v1.3.0
+1. 增加一个镜像浏览站点和一个下载站点(这是一个演示服务器，不可滥用，需手动开启)
+2. 修复插入失效
+3. 代码大改：修改生成插入的HTML代码的生成方式，方便以后增减站点
+
 v1.2.1-1.2.4
 1. 移除失效站点
 2. 添加警告（迟来的警告，希望没人遭受损失）
@@ -29,23 +62,15 @@ v1.0-1.1.4
 5. (原有)releases镜像下载按钮(releases Source code的快速下载1是没有加速效果的)
 6. 在克隆地址处插入克隆命令，不需要的可以编辑代码禁用
 
-下载安装地址：https://raw.githubusercontent.com/RC1844/FastGithub/master/FastGithub.js
+### 使用 Cloudflare 搭建Github镜像(实为代理)
+[基础教程](https://github.com/EtherDream/jsproxy/tree/master/cf-worker)
 
-或：[Greasyfork](https://greasyfork.org/zh-CN/scripts/397419-github-%E9%95%9C%E5%83%8F%E5%8A%A0%E9%80%9F%E4%B8%8B%E8%BD%BD)
-
-### 已知的GitHub镜像(含失效站点)
-|域名|https|克隆加速|zip加速|releases加速|主机服务商|服务器所在地
-|:---:|:----------:|:--------:|:---------:|:---------:|:---------:|:---------:|
-|https://github.com.cnpmjs.org|✓|?✓|✗|✗|dnspod|香港
-|~~http://github-mirror.bugkiller.org~~|✗|✓|✗|✓|?|日本
-|https://github.wuyanzheshui.workers.dev|✓|✗|✓|✓|Cloudflare|美国
-
-<figure class="half">
-    <img src="REANDME\releases1.png" width="600">
-    <img src="REANDME\releases2.png" width="600">
-</figure>
-<figure class="half">
-    <img src="REANDME\releases3.png" width="600">
-    <img src="REANDME\releases4.png" width="600">
-</figure>
-
+只需要将基础教程中[index.js](https://raw.githubusercontent.com/EtherDream/jsproxy/master/cf-worker/index.js)的代码：
+```
+const ASSET_URL = 'https://etherdream.github.io/jsproxy'
+```
+改为
+```
+const ASSET_URL = 'https://github.com'
+```
+即可
